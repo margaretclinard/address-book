f30e28956672b97b9c42a90b87328f7eddc3b99f
Margaret Clinard <margaret.clinard@gmail.com>
2015-02-10 08:01:25 -0600

    remove button functioning on page

--- public/js/main.js
+++ public/js/main.js
@@ -1,66 +1,68 @@
 /* jshint jquery: true */
 
-(function () {
-  'use strict';
+'use strict';
 
-  function hello() {
-    return 'world';
-  }
+function hello() {
+  return 'world';
+}
 
-  var FIREBASE_URL = 'https://mcaddressbook.firebaseio.com/contacts.json';
+var FIREBASE_URL = 'https://mcaddressbook.firebaseio.com/contacts.json',
+    $form = $('.contacts-form'),
+    $newContact = $('.newContact'),
+    $addContact = $('.addContact'),
+    $removeContact = $('.remove');
 
-  $(document).ready(function(evt) {
-    var $form = $('.contacts-form'),
-        $newContact = $('.newContact'),
-        $addContact = $('.addContact'),
-        $removeContact = $('.remove');
-
-    $newContact.click(function() {
-      console.log('clicked new contact');
-      $form.show();
-      $addContact.show();
-      $newContact.hide();
-    });
-
-    $addContact.click(getContact);
-    $('table').on('click', $removeContact, removeContact);
+$(document).ready(function () {
+  $newContact.click(function() {
+    $form.show();
+    $addContact.show();
+    $newContact.hide();
   });
 
-  $.get(FIREBASE_URL, function(data){
-    Object.keys(data).forEach(function(contact) {
-      addContactToTable(data[contact]);
-    });
+  $addContact.click(getContact);
+  removeContact();
+});
+
+$.get(FIREBASE_URL, function(data){
+  Object.keys(data).forEach(function(contact) {
+    addContactToTable(data[contact]);
   });
+});
 
-  function addContactToTable(contact) {
-    var $tr = $('<tr><td class="image"><img class="image" src="' + contact.photo +
-               '"></td><td class="name">' + contact.name +
-                '</td><td class="phone">' + contact.phone +
-                '</td><td class="email">' + contact.email +
-               '</td><td><button class="remove">Remove</button></td></tr>');
+function addContactToTable(contact) {
+  var $tr = $('<tr><td class="image"><img class="image" src="' + contact.photo +
+             '"></td><td class="name">' + contact.name +
+              '</td><td class="phone">' + contact.phone +
+              '</td><td class="email">' + contact.email +
+             '</td><td><button class="remove">Remove</button></td></tr>');
 
-    $('.target').append($tr);
-  }
+  $('.target').append($tr);
+}
 
-  function getContact(event) {
-    event.preventDefault();
+function getContact(event) {
+  event.preventDefault();
 
-    var $name = $('.name').val(),
-        $phone = $('.phone').val(),
-        $email = $('.email').val(),
-        $photo = $('.photo').val();
+  var $name = $('.name').val(),
+      $phone = $('.phone').val(),
+      $email = $('.email').val(),
+      $photo = $('.photo').val();
 
-    var contact = {name: $name, phone: $phone, email: $email, photo: $photo};
-    var data = JSON.stringify(contact);
-    $.post(FIREBASE_URL, data, function(res){
-    });
-  }
+  var contact = {name: $name, phone: $phone, email: $email, photo: $photo};
+  var data = JSON.stringify(contact);
+  $.post(FIREBASE_URL, data, function(res){
+    addContactToTable(contact);
+  });
+  $form.hide();
+  $addContact.hide();
+  $newContact.show();
+}
 
-  function removeContact() {
-    var $tr = $(this);
+function removeContact() {
+  $('tbody').on('click', $removeContact, function(evt){
+    var $tr = $(evt.target).closest('tr');
     $tr.remove();
-  }
-
-})();
-
-
+    //var uuid = $tr.data('uuid');
+    //var url = 'https://mcaddressbook.firebaseio.com/contacts' + uuid + '.json';
+    //$.ajax(url, {type: "DELETE"});
+  });
+}

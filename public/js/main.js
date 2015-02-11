47f85fbbf2ca5e9e4320ebc14cea1403ed4333d3
Margaret Clinard <margaret.clinard@gmail.com>
2015-02-10 18:25:45 -0600

    login and logout buttons work, register button does not

--- public/js/main.js
+++ public/js/main.js
@@ -2,11 +2,12 @@
 
 'use strict';
 
-var FIREBASE_URL = 'https://mcaddressbook.firebaseio.com/contacts.json',
+var FIREBASE_URL = 'https://mcaddressbook.firebaseio.com',
     $form = $('.contacts-form'),
     $newContact = $('.newContact'),
     $addContact = $('.addContact'),
-    $removeContact = $('.remove');
+    $removeContact = $('.remove'),
+    fb = new Firebase(FIREBASE_URL);
 
 $(document).ready(function () {
   $newContact.click(function() {
@@ -19,11 +20,71 @@ $(document).ready(function () {
   removeContact();
 });
 
-$.get(FIREBASE_URL, function(data){
-  Object.keys(data).forEach(function(uuid) {
-    addContactToTable(uuid, data[uuid]);
+if (fb.getAuth()) {
+  $('.login').remove();
+  $('.app').toggleClass('hidden');
+
+  $.get(FIREBASE_URL + '/users/' + fb.getAuth().uid + '/data/friends.json', function(data){
+    if(data !== null) {
+      Object.keys(data).forEach(function(uuid) {
+        addContactToTable(uuid, data[uuid]);
+      });
+    }
+  });
+}
+
+$('.login input[type="button"]').click(function (event) {
+  var $loginForm = $(event.target.closest('form')),
+      email      = $loginForm.find('[type="email"]').val(),
+      pass       = $loginForm.find('[type="password"]').val(),
+      data       = {email: email, password: pass};
+
+  registerAndLogin(data, function (err, auth) {
+    if (err) {
+      $('.error').text(err);
+    } else {
+      location.reload(true);
+    }
   });
 });
+
+$('.login form').submit(function(event){
+  var $loginForm = $(event.target),
+      email      = $loginForm.find('[type="email"]').val(),
+      pass       = $loginForm.find('[type="password"]').val(),
+      data       = {email: email, password: pass};
+
+  event.preventDefault();
+
+  fb.authWithPassword(data, function(err, auth) {
+    if (err) {
+      $('.error').text(err);
+    } else {
+      location.reload(true);
+    }
+  });
+});
+
+$('.logout').click(function (){
+  fb.unauth();
+  location.reload(true);
+});
+
+function registerAndLogin(obj, cb) {
+  fb.createUser(obj, function(err) {
+    if (!err) {
+      fb.authWithPassword(obj, function (err, auth){
+        if (!err) {
+          cb(null, auth);
+        } else {
+          cb(err);
+        }
+      });
+    } else {
+      cb(err);
+    }
+  });
+}
 
 function addContactToTable(uuid, contact) {
   var $tr = $('<tr><td class="image"><img class="image" src="' + contact.photo +
@@ -45,7 +106,7 @@ function getContact(event) {
 
   var contact = {name: $name, phone: $phone, email: $email, photo: $photo};
   var data = JSON.stringify(contact);
-  $.post(FIREBASE_URL, data, function(){
+  $.post(FIREBASE_URL + '/users/' + fb.getAuth().uid + '/data/friends.json', data, function(){
     addContactToTable(data.uuid, contact);
   });
   $form.hide();
@@ -58,7 +119,7 @@ function removeContact() {
     var $tr = $(evt.target).closest('tr');
     $tr.remove();
     var uuid = $tr.data('uuid');
-    var url = 'https://mcaddressbook.firebaseio.com/contacts/' + uuid + '.json';
+    var url = FIREBASE_URL + '/users/' + fb.getAuth().uid + '/data/friends/' + uuid + '.json';
     $.ajax(url, {type: "DELETE"});
   });
 }
